ee5252a64d668e7ef78ad96f0f824051cd167a88
witalij <w.koweschnikow@rejecty.com>
2023-08-14 16:15:42 +0200

    Fibonacci Folge bis 13

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace HelloWorld
+﻿using System.Diagnostics;
+
+namespace HelloWorld
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            //Fakultät
             int[] fak = new int[7];
             int seven = 7;
 
@@ -19,6 +22,24 @@
             }
 
             Console.WriteLine("7! = {0}", fakultat);
+
+            //Fibonacci
+            List<int> list = new List<int>();
+            list.Add(0);
+            list.Add(1);
+
+            while (list.Count < 8) {
+                int last = list.Last();
+                int lastlast = list[list.Count - 2];
+                list.Add(last + lastlast);
+            }
+
+            string fibonacci = "Fibonacci Folge bis 13: ";
+            foreach (int i in list)
+            {
+                fibonacci = fibonacci + i + ",";
+            }
+            Console.WriteLine(fibonacci);
         }
     }
 }
