49176610711b3a390846cd2362bf72d5defc4566
witalij <w.koweschnikow@rejecty.com>
2023-08-14 15:31:50 +0200

    remove unnecessary writelines

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,8 +4,6 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-
             int[] fak = new int[7];
             int seven = 7;
 
@@ -17,7 +15,6 @@
             int fakultat = 1;
             foreach (int i in fak)
             {
-                Console.WriteLine(i);  
                 fakultat *= i;
             }
 
