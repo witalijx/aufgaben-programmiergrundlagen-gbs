e853c53a371bd0cdb44db91add4152896f01ca5a
witalij <w.koweschnikow@rejecty.com>
2023-08-15 14:30:56 +0200

    no newlines before brackets

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,24 +1,19 @@
 ﻿using System.Diagnostics;
 using System.Runtime.InteropServices;
 
-namespace HelloWorld
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+namespace HelloWorld {
+    internal class Program {
+        static void Main(string[] args) {
             //Fakultät
             int[] fak = new int[7];
             int seven = 7;
 
-            for (int i = 0; i < seven; i++)
-            {
+            for (int i = 0; i < seven; i++) {
                 fak[i] = i + 1;
             }
 
             int fakultat = 1;
-            foreach (int i in fak)
-            {
+            foreach (int i in fak) {
                 fakultat *= i;
             }
 
@@ -29,39 +24,29 @@ namespace HelloWorld
             list.Add(0);
             list.Add(1);
 
-            while (list.Count < 8)
-            {
+            while (list.Count < 8) {
                 int last = list.Last();
                 int lastlast = list[list.Count - 2];
                 list.Add(last + lastlast);
             }
 
             string fibonacci = "Fibonacci Folge bis 13: ";
-            foreach (int i in list)
-            {
+            foreach (int i in list) {
                 fibonacci = fibonacci + i + ",";
             }
             Console.WriteLine(fibonacci);
 
-            static int gebeMaxAus(int int1, int int2, int int3)
-            {
-                if (int1 > int2)
-                {
-                    if (int1 > int3)
-                    {
+            static int gebeMaxAus(int int1, int int2, int int3) {
+                if (int1 > int2) {
+                    if (int1 > int3) {
                         return int1;
-                    } else
-                    {
+                    } else {
                         return int3;
                     }
-                } else
-                {
-                    if (int2 > int3)
-                    {
+                } else {
+                    if (int2 > int3) {
                         return int2;
-                    }
-                    else
-                    {
+                    } else {
                         return int3;
                     }
                 }
@@ -69,11 +54,9 @@ namespace HelloWorld
 
             Console.WriteLine("Grösste Zahl: {0}", gebeMaxAus(12, 34, 11));
 
-            static void gebeNullBisHunderAus()
-            {
+            static void gebeNullBisHunderAus() {
                 int iterator = 0;
-                while (iterator <= 100)
-                {
+                while (iterator <= 100) {
                     Console.WriteLine("Iterator: {0}", iterator);
                     iterator++;
                 }
